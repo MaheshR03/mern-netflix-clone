c6998ae9956822142e64077b189b31462fdb9114
Mahesh R <maheshrroy@gmail.com>
2024-11-12 23:56:37 +0530

    added correct github path

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -13,7 +13,7 @@ const Footer = () => {
 					</a>
 					. The source code is available on{" "}
 					<a
-						href='https://github.com/MaheshR03/Netflix-clone-mern'
+						href='https://github.com/MaheshR03/netflix-clone'
 						target='_blank'
 						rel='noreferrer'
 						className='font-medium underline underline-offset-4'
